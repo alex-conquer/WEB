7656cd05d7dcb34918c21bebf12e5066f145cca3
alex <tymin.a@bk.ru>
2024-04-02 19:41:35 +0300

    My_Work

---
+++ app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from app import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('hot/', views.hot, name='hot'),
+    path('questions/<int:question_id>', views.question, name='question'),
+    path('ask/', views.ask, name='ask'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login')
+]
