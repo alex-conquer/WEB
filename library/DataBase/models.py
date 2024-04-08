647c2402fc8a7711090427f98415ca7f0d9ef8a0
alex <tymin.a@bk.ru>
2024-04-08 12:32:42 +0300

    problems with base

--- library/DataBase/models.py
+++ library/DataBase/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import date
+from django.utils import timezone
+
+# class TopQuestionsManager(models.Manager):
+#     def get_queryset(self):
+#         # Возвращаем вопросы, отсортированные по числу лайков в убывающем порядке
+#         return super().get_queryset().annotate(num_likes=Count('likes')).order_by('-num_likes')[:10]
+
+# class TaggedQuestionsManager(models.Manager):
+#     def get_queryset(self, tag_name):
+#         # Возвращаем вопросы, отмеченные заданным тегом
+#         return super().get_queryset().filter(tags__name=tag_name)
 
 class User(models.Model):
     username_id = models.AutoField(primary_key=True)
@@ -10,11 +21,6 @@ class User(models.Model):
 
     def __str__(self):
         return self.first_name
-
-class Like(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, null=True, blank=True)
-    answer = models.ForeignKey(Answers, on_delete=models.CASCADE, null=True, blank=True)
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
@@ -30,20 +36,30 @@ class Question(models.Model):
     topic = models.CharField(max_length=120)
     likes = models.IntegerField()
     tags = models.ManyToManyField(Tag)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=timezone.now)
 
-    def __str__(self):
-        return self.topic
-
-    def clean(self):
-        if self.tags.count() > 5:
-            raise ValidationError("Максимальное число тегов 5")
+    # top_question = TopQuestionsManager()
+    # tagged_questions = TaggedQuestionsManager()
 
 class Answers(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     text = models.CharField(max_length=800)
     likes = models.IntegerField()
+    created_at = models.DateTimeField(default=timezone.now)
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, null=True, blank=True)
+    answer = models.ForeignKey(Answers, on_delete=models.CASCADE, null=True, blank=True)
+
+
+    def __str__(self):
+        return self.topic
+
+    # def clean(self):
+    #     if self.tags.count() > 5:
+    #         raise ValidationError({"tags": "Максимальное число тегов 5"})
 
 
     
--- library/DataBase/views.py
+++ library/DataBase/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Question
 
 # Create your views here.
+
+def new_questions(request):
+    new_questions = Question.objects.order_by('-created_at')[:5]
+    return render(request, 'new_questions.html', {'new_questions': new_questions})
+
+def popular_questions(request):
+    popular_questions = Question.objects.annotate(num_likes=models.Count('like')).order_by('-num_likes')[:5]
+    return render(request, 'popular_questions.html', {'popular_questions': popular_questions})
+
+def questions_by_tag(request, tag_name):
+    questions_by_tag = Question.objects.filter(tags__name=tag_name)
+    return render(request, 'questions_by_tag.html', {'questions_by_tag': questions_by_tag})
