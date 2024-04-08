e691fb8f976414aae373268388875d4083410713
alex <tymin.a@bk.ru>
2024-04-08 21:05:20 +0300

    answers ok, но надо связи продумать

--- library/DataBase/models.py
+++ library/DataBase/models.py
@@ -11,20 +11,18 @@ class TaggedQuestionsManager(models.Manager):
     def get_queryset(self, tag_name):
         return super().get_queryset().filter(tags__name=tag_name)
 
-    def __str__(self):
-        return self.first_name
-
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(null=True, blank=True)
 
 class Tag(models.Model):
     name = models.CharField(max_length=50)
+    
     def __str__(self):
         return self.name
     
 class Question(models.Model):
-    user = models.ForeignKey(Profile, on_delete=models.PROTECT)
+    profile = models.ForeignKey(Profile, on_delete=models.PROTECT)
     text = models.CharField(max_length=300)
     topic = models.CharField(max_length=120)
     likes = models.PositiveIntegerField(default=0)
@@ -36,13 +34,11 @@ class Question(models.Model):
     tagged_questions = TaggedQuestionsManager()
 
 class Answers(models.Model):
-    user = models.ForeignKey(Profile, on_delete=models.PROTECT)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.PROTECT)
+    # question = models.ForeignKey(Question, on_delete=models.CASCADE)
     text = models.CharField(max_length=800)
     likes = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(default=timezone.now)
 
 class Like(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, null=True, blank=True)
-    answer = models.ForeignKey(Answers, on_delete=models.CASCADE, null=True, blank=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
